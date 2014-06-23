bb04dab2c745f7c5195599b2ba6ed53296bded25
Cliff Curry <ccurry@google.com>
2014-06-22 20:35:44 -0700

    Initial Commit

---
+++ FacebookPost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FacebookPost
+//
+//  Created by Cliff Curry on 6/22/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FacebookPost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FacebookPost
+//
+//  Created by Cliff Curry on 6/22/14.
+//  Copyright (c) 2014 Cliff Curry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
